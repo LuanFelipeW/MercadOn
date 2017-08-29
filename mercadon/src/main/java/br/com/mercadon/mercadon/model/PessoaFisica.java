851c63cf7ebdc8b3ea4317daaff97fa5d7fc918b
Noturno <Noturno@192.168.7.180>
2017-08-29 19:19:03 -0300

    CRIAÇÃO PESSOA FISICA

--- mercadon/src/main/java/br/com/mercadon/mercadon/model/PessoaFisica.java
+++ mercadon/src/main/java/br/com/mercadon/mercadon/model/PessoaFisica.java
@@ -1,5 +1,85 @@
 package br.com.mercadon.mercadon.model;
 
+import java.util.Date;
+
 public class PessoaFisica {
+	// ATRIBUTOS
+	private Integer codigo;
+	private String nome;
+	private String sobrenome;
+	private String email;
+	private Date dataNasc;
+	private String sexo;
+	private PessoaJuridica pessoaJuridica;
+
+	// MÉTODO CONSTRUTOR
+	public PessoaFisica(Integer codigo, String nome, String sobrenome, String email, Date dataNasc, String sexo,
+			PessoaJuridica pessoaJuridica) {
+		super();
+		this.codigo = codigo;
+		this.nome = nome;
+		this.sobrenome = sobrenome;
+		this.email = email;
+		this.dataNasc = dataNasc;
+		this.sexo = sexo;
+		this.pessoaJuridica = pessoaJuridica;
+	}
+
+	// GETTER E SETTER
+	public Integer getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getDataNasc() {
+		return dataNasc;
+	}
+
+	public void setDataNasc(Date dataNasc) {
+		this.dataNasc = dataNasc;
+	}
+
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+
+	public PessoaJuridica getPessoaJuridica() {
+		return pessoaJuridica;
+	}
+
+	public void setPessoaJuridica(PessoaJuridica pessoaJuridica) {
+		this.pessoaJuridica = pessoaJuridica;
+	}
 
 }
---
+++ mercadon/src/main/java/br/com/mercadon/mercadon/model/PessoaJuridica.java
@@ -0,0 +1,7 @@
+package br.com.mercadon.mercadon.model;
+
+public class PessoaJuridica {
+	
+	Integer
+
+}
