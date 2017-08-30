dc487fbb9b253ca3bb823430f09cf012a2ffd91b
Noturno <Noturno@192.168.7.21>
2017-08-30 20:34:19 -0300

    backup

--- mercadon/src/main/java/br/com/grupo4/mercadon/service/SubCategoriaService.java
+++ mercadon/src/main/java/br/com/grupo4/mercadon/service/SubCategoriaService.java
@@ -1,5 +1,28 @@
 package br.com.grupo4.mercadon.service;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+import br.com.grupo4.mercadon.jdbc.oracle.ConnectionPoolOracle;
+import br.com.grupo4.mercadon.model.SubCategoria;
+import br.com.grupo4.mercadon.DAO.SubCategoriaDAO;
+
+
 public class SubCategoriaService {
 
+	
+	//Metodo para listar todas as subcategorias
+	public List<SubCategoria> listarSubCategorias() throws SQLException{
+		try (Connection con = new ConnectionPoolOracle().getConnection()) {
+            return new SubCategoriaDAO(con).lista();
+		}
+	}
+	//Metodo para buscar todas as subcategorias --- arrumar
+	public List<SubCategoria> buscarSubCategoria() throws SQLException{
+		try (Connection con = new ConnectionPoolOracle().getConnection()) {
+            return new SubCategoriaDAO(con).lista();
+            
+}
+}
 }
