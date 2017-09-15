ab0937954da13320c6d6accb0bff82416a4222a4
Noturno <Noturno@192.168.7.19>
2017-09-15 20:33:10 -0300

    backup

--- mercadon/src/main/java/br/com/grupo4/mercadon/service/ProdutoService.java
+++ mercadon/src/main/java/br/com/grupo4/mercadon/service/ProdutoService.java
@@ -2,14 +2,16 @@ package br.com.grupo4.mercadon.service;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.text.DecimalFormat;
 import java.util.List;
 
 import br.com.grupo4.mercadon.jdbc.oracle.ConnectionPoolOracle;
 import br.com.grupo4.mercadon.model.Produto;
-import br.com.grupo4.mercadon.DAO.ProdutoDAO;;
+import br.com.grupo4.mercadon.DAO.ProdutoDAO;
+import br.com.grupo4.mercadon.DAO.SubCategoriaDAO;;
 
 public class ProdutoService {
-
+DecimalFormat df = new DecimalFormat("#0.00");
 
 	//metodo listagem de Produtos - como Filtro
 	public List<Produto> listarProdutos() throws SQLException{
@@ -18,10 +20,14 @@ public class ProdutoService {
 		}
 	}
 	//metodo busca de Produtos - como Pesquisa
-	public List<Produto> buscarProdutos() throws SQLException{
+	public void buscarListaProduto(String entrada) throws SQLException{
 		try (Connection con = new ConnectionPoolOracle().getConnection()) {
-            return new ProdutoDAO(con).lista();
+			List<Produto> produtos = new ProdutoDAO(con).buscarListaProduto(entrada);
+			for (Produto produto : produtos) {
+				System.out.println(produto.getNome() +"\n"+ df.format((produto.getPreco())));
             
 }
 }
+}
+	
 }
--- mercadon/src/main/java/testes/teste.java
+++ mercadon/src/main/java/testes/teste.java
@@ -3,24 +3,22 @@ package testes;
 import java.sql.SQLException;
 
 import br.com.grupo4.mercadon.service.CategoriaService;
+import br.com.grupo4.mercadon.service.ProdutoService;
 import br.com.grupo4.mercadon.service.SubCategoriaService;
 
 public class teste {
 	public static void main(String[] args) throws SQLException {
 
-		SubCategoriaService scat = new SubCategoriaService();
+		/*SubCategoriaService scat = new SubCategoriaService();
 		scat.buscarSubCategoriaProdutos(60);
 
 		System.out.println("|-----------------------------------------------------------------|");
 
 		CategoriaService cat = new CategoriaService();
-		cat.buscarCategoriaProdutos(1);
+		cat.buscarCategoriaProdutos(1);*/
 
-		/*
-		 * CategoriaService cat = new CategoriaService();
-		 * 
-		 * cat.buscarCategoriaProdutos(1);
-		 */
+		ProdutoService pros = new ProdutoService();
+		pros.buscarListaProduto("bolo");
 
 	}
 }
