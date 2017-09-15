5cc43dc4326dc2e4f7fe110a349d15bade13d7d9
Noturno <Noturno@192.168.7.177>
2017-09-15 20:41:06 -0300

    asd

--- mercadon/src/main/java/br/com/grupo4/mercadon/DAO/ProdutoDAO.java
+++ mercadon/src/main/java/br/com/grupo4/mercadon/DAO/ProdutoDAO.java
@@ -43,51 +43,7 @@ public class ProdutoDAO {
 
 		String sql = " SELECT PRO_NOME, PRO_PRECO ";
 		sql += " FROM PRODUTO ";
-		sql += " ORDER BY PRO_NOME ? ";
-		try (PreparedStatement stmt = conn.prepareStatement(sql)) {
-			stmt.setInt(1, entrada);
-			stmt.execute();
-			try (ResultSet rs = stmt.getResultSet()) {
-				while (rs.next()) {
-					String nome = rs.getString(1);
-					double preco = rs.getDouble(2);
-					Produto produto = new Produto(nome, preco);
-					lProduto.add(produto);
-
-				}
-			}
-		}
-		return lProduto;
-	}
-
-	public List<Produto> filtrarProdutoZA(Integer entrada) throws SQLException {
-		List<Produto> lProduto = new ArrayList<>();
-
-		String sql = " SELECT PRO_NOME, PRO_PRECO ";
-		sql += " FROM PRODUTO ";
-		sql += " ORDER BY PRO_NOME ? ";
-		try (PreparedStatement stmt = conn.prepareStatement(sql)) {
-			stmt.setInt(1, entrada);
-			stmt.execute();
-			try (ResultSet rs = stmt.getResultSet()) {
-				while (rs.next()) {
-					String nome = rs.getString(1);
-					double preco = rs.getDouble(2);
-					Produto produto = new Produto(nome, preco);
-					lProduto.add(produto);
-
-				}
-			}
-		}
-		return lProduto;
-	}
-
-	public List<Produto> filtrarProdutoPreco(Integer entrada) throws SQLException {
-		List<Produto> lProduto = new ArrayList<>();
-
-		String sql = " SELECT PRO_NOME, PRO_PRECO ";
-		sql += " FROM PRODUTO ";
-		sql += " ORDER BY PRO_PRECO ? ";
+		sql += " ORDER BY ? ";
 		try (PreparedStatement stmt = conn.prepareStatement(sql)) {
 			stmt.setInt(1, entrada);
 			stmt.execute();
